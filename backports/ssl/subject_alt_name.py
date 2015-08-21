5aa4cb08836778c20e0165799bddbc56ac2bb1a3
Rian Hunter <rian@alum.mit.edu>
2015-08-21 02:21:49 -0700

    Compare ext_name against b'subjectAltName'
    In Python3, `ext.get_short_name()` returns a bytes type.
    Make sure we compare against the right type.

--- backports/ssl/subject_alt_name.py
+++ backports/ssl/subject_alt_name.py
@@ -133,7 +133,7 @@ def get_subject_alt_name(peer_cert):
     for i in range(peer_cert.get_extension_count()):
         ext = peer_cert.get_extension(i)
         ext_name = ext.get_short_name()
-        if ext_name != 'subjectAltName':
+        if ext_name != six.b('subjectAltName'):
             continue
 
         # PyOpenSSL returns extension data in ASN.1 encoded form
